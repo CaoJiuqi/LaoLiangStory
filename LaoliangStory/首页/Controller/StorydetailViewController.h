84292fa8f8d318be5629a3704afcfea7a2fa6d84
zjq <ye136580@163.com>
2015-10-20 20:37:13 +0800

    详情页面

---
+++ LaoliangStory/首页/Controller/StorydetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  StorydetailViewController.h
+//  LaoliangStory
+//
+//  Created by zjq on 15-10-20.
+//  Copyright (c) 2015年 tens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StorydetailViewController : UIViewController
+
+@end
---
+++ LaoliangStory/首页/Controller/StorydetailViewController.m
@@ -0,0 +1,37 @@
+//
+//  StorydetailViewController.m
+//  LaoliangStory
+//
+//  Created by zjq on 15-10-20.
+//  Copyright (c) 2015年 tens. All rights reserved.
+//
+
+#import "StorydetailViewController.h"
+
+@interface StorydetailViewController ()
+
+@end
+
+@implementation StorydetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
